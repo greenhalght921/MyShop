e81d097b008511e9557c6b1f83f59add8c95591f
Tom Greenhalgh <greenhalght921@gmail.com>
2018-01-31 21:19:30 +0000

    OK So Far

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -18,5 +18,7 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketsItems { get; set; }
     }
 }
